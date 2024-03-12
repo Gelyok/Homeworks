04e42f37688f01619ad4b9dc2bd5510973274145
Neto test <angelina.rubantsova@gmail.com>
2024-03-12 19:03:09 +0100

    Update

--- Installation/src/Main.java
+++ Installation/src/Main.java
@@ -1,5 +1,12 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.*;
+import java.nio.file.*;
+
+import static java.nio.file.Files.createDirectory;
+import static java.nio.file.Files.createFile;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -10,22 +17,30 @@ public class Main {
         File savegamesFolder = new File(gamesFolder, "savegames");
         File tempFolder = new File(gamesFolder, "temp");
 
-        createDirectory(gamesFolder);
-        createDirectory(srcFolder);
-        createDirectory(resFolder);
-        createDirectory(savegamesFolder);
-        createDirectory(tempFolder);
+        if (gamesFolder.mkdirs() &&
+                srcFolder.mkdirs() &&
+                resFolder.mkdirs() &&
+                savegamesFolder.mkdirs() &&
+                tempFolder.mkdirs()) {
 
-        File mainFolder = new File(srcFolder, "main");
-        createDirectory(mainFolder);
-        createFile(new File(mainFolder, "Main.java"));
-        createFile(new File(mainFolder, "Utils.java"));
+            File mainFolder = new File(srcFolder, "main");
+            mainFolder.mkdirs();
+            try {
+                Files.createFile(Paths.get(mainFolder.getAbsolutePath(), "Main.java"));
+                Files.createFile(Paths.get(mainFolder.getAbsolutePath(), "Utils.java"));
+                Files.createFile(Paths.get(tempFolder.getAbsolutePath(), "temp.txt"));
+            } catch (IOException e) {
+                System.err.println("Не удалось создать файл: " + e.getMessage());
+            }
 
-        createDirectory(new File(resFolder, "drawables"));
-        createDirectory(new File(resFolder, "vectors"));
-        createDirectory(new File(resFolder, "icons"));
+            new File(resFolder, "drawables").mkdirs();
+            new File(resFolder, "vectors").mkdirs();
+            new File(resFolder, "icons").mkdirs();
 
-        createFile(new File(tempFolder, "temp.txt"));
+        } else {
+            System.err.println("Не удалось создать одну или несколько директорий");
+        }
+
 
         GameProgress game1 = new GameProgress(100, 3, 10, 150.5);
         GameProgress game2 = new GameProgress(90, 4, 15, 200.0);
@@ -42,25 +57,31 @@ public class Main {
 
     }
 
-    private static void createDirectory(File directory) {
+    private static boolean checkIsCreateDirectory(File directory) {
         if (!directory.exists()) {
-            if (directory.mkdir()) {
+            if (directory.mkdirs()) {
                 System.out.println("Директория " + directory.getName() + " успешно создана.");
+                return true;
             } else {
                 System.err.println("Ошибка при создании директории " + directory.getName());
+                return false;
             }
         }
+        return false;
     }
 
-    private static void createFile(File file) {
+    private static boolean checkAndCreateFile(File file) {
         try {
             if (file.createNewFile()) {
                 System.out.println("Файл " + file.getName() + " успешно создан.");
+                return true;
             } else {
-                System.err.println("Ошибка при создании файла " + file.getName());
+                System.err.println("Ошибка при создании файла " + file.getName() + ". Файл уже существует.");
+                return false;
             }
         } catch (IOException e) {
             System.err.println("Ошибка при создании файла " + file.getName() + ": " + e.getMessage());
+            return false;
         }
     }
 
@@ -74,7 +95,7 @@ public class Main {
         }
     }
 
-    private static void writeLogFile(String log, File file) {
+    private static void writeLogToFile(String log, File file) {
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(log);
             System.out.println("Лог успешно записан в файл " + file.getName());
@@ -84,24 +105,32 @@ public class Main {
     }
 
     private static void createZipArchive(String sourceFolderPath, String zipFilePath) {
+        File sourceFolder = new File(sourceFolderPath);
+        List<File> sourceFiles = Arrays.asList(sourceFolder.listFiles());
+
         try {
             FileOutputStream fos = new FileOutputStream(zipFilePath);
             ZipOutputStream zos = new ZipOutputStream(fos);
-            File sourceFolder = new File(sourceFolderPath);
-            addFilesToZip(sourceFolder, sourceFolder.getName(), zos);
+
+            for (File sourceFile : sourceFiles) {
+                addFilesToZip(sourceFile, sourceFile.getName(), zos);
+            }
+
             zos.close();
             fos.close();
-
             System.out.println("Архив успешно создан: " + zipFilePath);
         } catch (IOException e) {
-            System.err.println("Ошибка создания архива zip:" + e.getMessage());
+            System.err.println("Ошибка при создании архива zip: " + e.getMessage());
         }
     }
 
     private static void addFilesToZip(File sourceFile, String parentPath, ZipOutputStream zos) throws IOException {
         if (sourceFile.isDirectory()) {
-            for (File file : sourceFile.listFiles()) {
-                addFilesToZip(file, parentPath + File.separator + file.getName(), zos);
+            File[] files = sourceFile.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    addFilesToZip(file,  parentPath + File.separator + file.getName(), zos);
+                }
             }
         } else {
             byte[] buffer = new byte[1024];
@@ -118,9 +147,10 @@ public class Main {
 
     private static void deleteOriginalSaveFiles(String folderPath) {
         File folder = new File(folderPath);
-        for (File file : folder.listFiles()) {
-            if (file.isFile()) {
-                if (!file.getName().equals("savegames.zip")) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && !file.getName().equals("savegames.zip")) {
                     if (file.delete()) {
                         System.out.println("Удален файл: " + file.getName());
                     } else {
