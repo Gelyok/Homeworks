085bcf98c02e000ad17a37fb6cfd6821e0ef401a
Neto test <angelina.rubantsova@gmail.com>
2024-03-18 17:41:14 +0100

    Removing unnecessary lines of code

--- Installation/src/Main.java
+++ Installation/src/Main.java
@@ -23,43 +23,10 @@ public class Main {
                 "C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\savegames",
                 "C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\temp");
 
-        for (String dir : listDirectories) {
-            createDirectory(dir);
-        }
 
         createFile("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\src\\", "Main.java");
         createFile("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\src\\", "Util.java");
         createFile("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\src\\", "temp.txt");
-
-        File gamesFolder = new File("Games");
-        File srcFolder = new File(gamesFolder, "src");
-        File resFolder = new File(gamesFolder, "res");
-        File savegamesFolder = new File(gamesFolder, "savegames");
-        File tempFolder = new File(gamesFolder, "temp");
-
-        boolean allDirectoriesCreated = checkIsCreateDirectory(gamesFolder) &&
-                checkIsCreateDirectory(srcFolder) &&
-                checkIsCreateDirectory(resFolder) &&
-                checkIsCreateDirectory(savegamesFolder) &&
-                checkIsCreateDirectory(tempFolder);
-        if (allDirectoriesCreated) {
-            File mainFolder = new File(srcFolder, "main");
-            mainFolder.mkdirs();
-            try {
-                createFile(mainFolder.getAbsolutePath(), "Main.java");
-                createFile(mainFolder.getAbsolutePath(), "Utils.java");
-                createFile(tempFolder.getAbsolutePath(), "temp.txt");
-            } catch (IOException e) {
-                System.err.println("Не удалось создать файл: " + e.getMessage());
-            }
-
-            new File(resFolder, "drawables").mkdirs();
-            new File(resFolder, "vectors").mkdirs();
-            new File(resFolder, "icons").mkdirs();
-
-        } else {
-            System.err.println("Не удалось создать одну или несколько директорий");
-        }
 
 
         GameProgress game1 = new GameProgress(100, 3, 10, 150.5);
@@ -70,54 +37,33 @@ public class Main {
         saveGame("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\savegames\\game2.dat", game2);
         saveGame("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\savegames\\game3.dat", game3);
 
-        String zipFilePath = "C:\\Users\\HP\\Desktop\\savegames.zip";
-        createZipArchive("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\savegames\\", zipFilePath);
-
-        deleteOriginalSaveFiles(savegamesFolder.getAbsolutePath());
-        writeLogToFile(stringBuilder.toString(), new File("C:\\Users\\HP\\IdeaProjects\\Installation\\temp.txt"));
-
-
-        File file = new File("C:\\Users\\HP\\IdeaProjects\\Installation\\Games\\src\\Main.java");
-        if (file.canRead() && file.canWrite()) {
-            System.out.println("Файл доступен для чтения и записи.");
-        } else {
-            System.out.println("Невозможно прочитать или записать файл.");
-        }
     }
 
-    private static void createDirectory(String path) {
-        File f = new File(path);
-        if (!f.exists()) {
-            if (f.mkdirs()) {
-                stringBuilder.append("Директория " + f + "успешно создана: ");
-            } else {
-                stringBuilder.append("Ошибка при создании: " + f.getName());
-            }
-        } else stringBuilder.append("Директория " + path + "уже создана.");
-    }
-
+    //Проверяет существование и создает директорию, если ее нет
     private static boolean checkIsCreateDirectory(File directory) {
+        File f = new File(directory.getPath());
         if (!directory.exists()) {
             if (directory.mkdirs()) {
-                stringBuilder.append("Директория" + directory.getName() + "успешно создана\n");
+                stringBuilder.append("Директория" + directory.getName() + " успешно создана\n");
                 return true;
             } else {
                 stringBuilder.append("Ошибка при создании директории " + directory.getName() + "\n");
                 return false;
             }
         } else {
-            stringBuilder.append("Директория " + directory.getName() + "уже существует\n");
+            stringBuilder.append("Директория " + directory.getName() + " уже существует\n");
             return true;
         }
     }
 
+    //Проверяет существование и создает файл, если его нет
     private static boolean checkAndCreateFile(File file) {
         try {
             if (file.createNewFile()) {
-                System.out.println("Файл " + file.getName() + "успешно создан");
+                System.out.println("Файл " + file.getName() + " успешно создан");
                 return true;
             } else {
-                System.err.println("Ошибка при создании файла " + file.getName() + "Фвйлл уже существует");
+                System.err.println("Ошибка при создании файла " + file.getName() + " Файл уже существует");
                 return false;
             }
         } catch (IOException e) {
@@ -126,6 +72,7 @@ public class Main {
         }
     }
 
+    //Создает файл по указанному пути и имени
     private static void createFile(String path, String fileName) throws FileNotFoundException {
         File f = new File(path, fileName);
         try {
@@ -135,27 +82,14 @@ public class Main {
                 } else {
                     stringBuilder.append("Ошибка при создании файла ").append(f.getName()).append("\n");
                 }
-            } else stringBuilder.append("Файл " + fileName + "уже был создан ");
+            } else stringBuilder.append("Файл " + fileName + " уже был создан ");
         } catch (IOException e) {
             stringBuilder.append("Ошибка при создании файла ").append(f.getName()).append(": ").append(e.getMessage()).append("\n");
             throw new FileNotFoundException();
         }
     }
 
-    private static void createFileOrDirectory(String path) {
-        File f = new File(path);
-        if (!f.exists()) {
-            if (f.mkdirs()) {
-                stringBuilder.append("Директория " + f + "успешно создана: ");
-            } else {
-                stringBuilder.append("Ошибка при создании: " + f.getName());
-            }
-        } else {
-            stringBuilder.append("Директория уже существует: " + f.getName());
-        }
-    }
-
-
+    //Сохраняет игровой процесс в файле
     private static void saveGame(String filePath, GameProgress game) {
         try (FileOutputStream fos = new FileOutputStream(filePath);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
@@ -166,6 +100,7 @@ public class Main {
         }
     }
 
+    //Записывает лог в файл
     private static void writeLogToFile(String log, File file) {
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(stringBuilder.toString());
@@ -175,7 +110,7 @@ public class Main {
         }
     }
 
-
+    //Создает zip-архив из указанной директории и сохраняет его по указанному пути
     private static void createZipArchive(String sourceFolderPath, String zipFilePath) {
         File sourceFolder = new File(sourceFolderPath);
         if (!sourceFolder.exists() || !sourceFolder.isDirectory()) {
@@ -201,6 +136,7 @@ public class Main {
         }
     }
 
+    //Добавляет файлы в zip-архив
     private static void addFilesToZip(File sourceFile, String parentPath, ZipOutputStream zos) throws IOException {
         if (sourceFile.isDirectory()) {
             File[] files = sourceFile.listFiles();
@@ -222,6 +158,7 @@ public class Main {
         }
     }
 
+    //Удаляет оригинальные файлы сохранения
     private static void deleteOriginalSaveFiles(String folderPath) {
         File folder = new File(folderPath);
         if (!folder.exists() || !folder.isDirectory()) {
@@ -238,14 +175,11 @@ public class Main {
                     } else {
                         System.err.println("Не удалось удалить файл: " + file.getName());
                     }
-                    try (FileOutputStream fos = new FileOutputStream("C:\\Users\\HP\\IdeaProjects\\installation\\Games\\sro\\temp.txt")) {
-                        fos.write(stringBuilder.toString().getBytes());
-                    } catch (IOException ex) {
-                        System.out.println(ex.getMessage());
-                    }
                 }
             }
         }
+
+        writeLogToFile(stringBuilder.toString(), new File("C:\\Users\\HP\\IdeaProjects\\Installation\\temp.txt"));
     }
 }
 
